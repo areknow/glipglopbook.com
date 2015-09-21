d20cf2353b132122f064bffe934a72ffb8d7ec55
arnaudcrowther <arnaudcrowther@gmail.com>
2015-09-20 22:47:27 -0400

    second commit, repo redo

---
+++ index.php
@@ -0,0 +1,166 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'/>
+    <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no'>
+    <meta name='author' content='Arnaud Crowther, Alex Osbourne, Amanda Labelle, Eric Maul'>
+    <title>Glip Glop | Home</title>
+    <link rel="stylesheet" href="materialize/css/materialize.min.css" type="text/css">
+    <link rel="stylesheet" href="plugins/owl.carousel/assets/owl.carousel.css" type="text/css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href='plugins/black-tie/css/black-tie.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="css/animate.css" type="text/css">
+    <link rel="stylesheet" href="css/base.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+  </head>
+  <body class="front">
+    <ul id="slide-out" class="side-nav">
+      <div class="side-menu-header">Glip Glop</div>
+      <li><a href="#!">NEW ARRIVALS</a></li>
+      <li><a href="#!">BEST SELLERS</a></li>
+      <li><a href="#!">SEE ALL BOOK</a></li>
+    </ul>
+    <div class="navigation-bar">
+      <div class="logo">
+        <a href="#" class="button-collapse block" data-activates="slide-out"><i class="material-icons">menu</i></a>
+        <div class="block glip">Glip Glop</div>
+      </div>
+      <div class="menu">
+        <div id="btn-search" class="block"><i class="material-icons">search</i></div>
+        <div id="search-cont" class="searchbar"><input id="search-input" type="text"></div>
+        <div class="block"><i class="material-icons">home</i></div>
+        <div id="btn-account" class="block"><i class="material-icons">account_circle</i></div>
+      </div>
+      <div class="drop-down-login">
+        <div><input placeholder="Email" type="text"></div>
+        <div><input placeholder="Password" type="text"></div>
+        <div><button>ENTER</button></div>
+        <div class="help-row">
+          <a class="new" href="#!">new account</a>
+          <a class="help" href="#!">help?</a>
+        </div>
+      </div>
+    </div>
+    <div class="header-slider">
+      <div class="owl-carousel">
+        <div class="item slide1">
+          <div class="slide-text">
+            <div class="title">Save your money</div>
+            <div class="sub">Use your money on things that really matter. We help you keep your money in the bank.</div>
+          </div>
+        </div>
+        <div class="item slide2">
+          <div class="slide-text">
+            <div class="title">Make Connections</div>
+            <div class="sub">Get acquainted with classmates on your campus that are also selling school books.</div>
+          </div>
+        </div>
+        <div class="item slide3">
+          <div class="slide-text">
+            <div class="title">Study Harder</div>
+            <div class="sub">Spend less time worrying about getting your books, and more time focusing on your grades.</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <section id="about">
+      <h1>How does Glip Glop work?</h1>
+      <div class="row circle-row">
+        <div class="col l4 m4 s12">
+          <div id="btn-about1" class="about-circle search active-circle"><div class="inner">Search</div></div>
+        </div>
+        <div class="col l4 m4 s12">
+          <div id="btn-about2" class="about-circle meetup"><div class="inner">Meet Up</div></div>
+        </div>
+        <div class="col l4 m4 s12">
+          <div id="btn-about3" class="about-circle exchange"><div class="inner">Exchange</div></div>
+        </div>
+      </div>
+      <div class="row tab-row">
+        <div class="nubbin"></div>
+        <div id="about-tab1" class="about-tab active-tab">
+          <h2>Search for what you need</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div id="about-tab2" class="about-tab">
+          <h2>Meet up with classmates and colleagues</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div id="about-tab3" class="about-tab">
+          <h2>Exchange, sell, ship and receive textbooks</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+    </section>
+    <section id="started">
+      <h1>How do I get started?</h1>
+      <div class="row started-row">
+        <div class="col l6 m6 s12 first">
+          <p>Make a new account</p>
+          <div class="input-field">
+            <i class="material-icons prefix">account_circle</i>
+            <input id="icon_prefix" type="text" class="validate">
+            <label for="icon_prefix">First Name</label>
+          </div>
+          <div class="input-field">
+            <i class="material-icons prefix">account_circle</i>
+            <input id="icon_prefix" type="text" class="validate">
+            <label for="icon_prefix">Last Name</label>
+          </div>
+          <div class="input-field">
+            <i class="material-icons prefix">swap_vertical_circle</i>
+            <input id="icon_prefix" type="text" class="validate">
+            <label for="icon_prefix">Email</label>
+          </div>
+          <div class="input-field">
+            <i class="material-icons prefix">check_circle</i>
+            <input id="icon_prefix" type="text" class="validate">
+            <label for="icon_prefix">Password</label>
+          </div>
+          <div><a class="waves-effect waves-light btn-flat">GO</a></div>
+        </div>
+        <div class="col l6 m6 s12 links">
+          <p>Learn more about Glip Glop</p>
+          <div>
+            <a href="#!" class="waves-effect waves-light">
+              <i class="material-icons prefix">search</i>Search for a book</a>
+            <a href="#!" class="waves-effect waves-light">
+              <i class="material-icons prefix">live_help</i>Read the FAQ</a>
+            <a href="#!" class="waves-effect waves-light">
+              <i class="material-icons prefix">school</i>Terms of Service</a>
+            <a href="#!" class="waves-effect waves-light">
+              <i class="fab fab-github"></i>GitHub Documentation</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="footer">
+      <div class="row">
+        <div class="col l6">
+          <h3>Glip Glop</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div class="col l3">
+          <h3>Helpful links</h3>
+          <a>My Account</a>
+          <a>Privacy Policy</a>
+          <a>Terms of Service</a>
+          <a>Cookies Policy</a>
+        </div>
+        <div class="col l3">
+        <h3>Helpful links</h3>
+          <a>My Account</a>
+          <a>Privacy Policy</a>
+          <a>Terms of Service</a>
+          <a>Cookies Policy</a>
+        </div>
+      </div>
+    </section>
+  </body>
+  <script type="text/javascript" src="js/jquery.1.11.3.min.js"></script>
+  <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+  <script type="text/javascript" src="plugins/owl.carousel/owl.carousel.min.js"></script>
+<!--  <script type="text/javascript" src="js/wow.min.js"></script>-->
+  <script type="text/javascript" src="js/parallax.min.js"></script>
+  <script type="text/javascript" src="js/init.js"></script>
+</html>
---
+++ js/init.js
@@ -0,0 +1,172 @@
+$(function() {
+  
+  //init circles
+  aboutCircleSize();
+  
+//  $('select').material_select();
+  
+  $('#started').parallax({
+    speed: 0.2,
+    bleed: 30,
+    naturalWidth: 1440,
+    naturalHeight: 960,
+    iosFix: true,
+    androidFix: true,
+    imageSrc: 'res/parallax/baloons.jpg'
+  });
+  
+  //about button click
+  $("#btn-about1").click(function(event){showTab(1);});
+  $("#btn-about2").click(function(event){showTab(2);});
+  $("#btn-about3").click(function(event){showTab(3);});
+  
+      
+  //open search bar
+  $("#btn-search").click(function(event){
+    event.preventDefault();
+    if ( $(this).hasClass("isDown") ) {
+      $("#search-input").stop().animate({width:"0"}, 200);    
+      $("#btn-search").removeClass('white-button');
+    } 
+    else {
+      $("#search-input").stop().animate({width:"150"}, 200, function() {
+        $("#search-input").focus();                
+        $("#btn-search").addClass('white-button');           
+      });
+    }
+    $(this).toggleClass("isDown");
+    return false;
+  });
+  
+  
+  //account drop down
+  $("#btn-account").click(function() {
+    event.preventDefault();
+    if ( $(".drop-down-login").hasClass("isDown") ) {
+      $(".drop-down-login").fadeToggle();
+      $(".drop-down-login").removeClass("isDown");
+      $("#btn-account").removeClass('white-button');
+    } 
+    else {
+      $(".drop-down-login").fadeToggle();
+      $(".drop-down-login").addClass("isDown");
+      $("#btn-account").addClass('white-button');
+    }
+    return false;
+  });
+  
+  
+  
+  //close button clicks on body event
+  $(document).click(function(e){
+    if( $(e.target).closest("#search-cont").length > 0 ) {
+      return false;
+    }
+    if( $(e.target).closest(".drop-down-login").length > 0 ) {
+      return false;
+    }
+    if ( $("#btn-search").hasClass("isDown") ) {
+      $("#btn-search").click();
+    }
+    if ( $(".drop-down-login").hasClass("isDown") ) {
+      $("#btn-account").click();
+    }
+  });
+
+
+  
+  
+  
+
+  //side nav init
+  $('.button-collapse').sideNav({
+      menuWidth: 300,
+      edge: 'left', 
+      closeOnClick: true
+    }
+  );
+
+
+  
+  
+  
+  
+  //home page slider
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:7000,
+    items:1
+  });
+  
+  
+  
+  //animate on scroll initiate
+//  new WOW().init();
+  
+});
+
+
+
+
+
+
+
+
+
+
+
+
+$(window).resize(function() {
+  aboutCircleSize();
+  moveNubbin(activeTab(),"snap");
+});
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+function aboutCircleSize() {
+  $(".about-circle").height($(".about-circle").width());
+}
+function hideTabs() {
+  for (x = 1; x < 4; x++) { 
+    $('#about-tab'+x).hide();
+    $('#about-tab'+x).removeClass('active-tab');
+    $('#btn-about'+x).removeClass('active-circle');
+  }
+}
+function showTab(i) {
+  hideTabs();
+  $('#about-tab'+i).fadeIn();
+  $('#about-tab'+i).addClass('active-tab');
+  $('#btn-about'+i).addClass('active-circle');
+  moveNubbin(i,"anim");
+}
+function activeTab() {
+  for (x = 1; x < 4; x++) { 
+    if ($('#about-tab'+x).hasClass('active-tab'))
+      return x;
+  }
+}
+function moveNubbin(i,type) {
+  var offset = $('#btn-about'+i).offset();
+  var width = $('#btn-about'+i).width();
+  if(type=="anim") {
+    $('.nubbin').animate({
+      left: offset.left + ((width/2)-6)
+    });
+  }
+  if(type=="snap") {
+    $('.nubbin').css({
+      left: offset.left + ((width/2)-6)
+    });
+  }
+}
