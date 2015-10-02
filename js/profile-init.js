e204513c0343272c8aab8fe54fd45e1ef292ef4d
arnaudcrowther <arnaudcrowther@gmail.com>
2015-10-02 09:38:37 -0400

    minor UI changes

--- js/profile-init.js
+++ js/profile-init.js
@@ -3,22 +3,27 @@ $(function() {
   //profile form
   $("#btn-profile-save").click(function(){
     
-    var mydata = $("form#frm-profile").serialize();
-    $.ajax({
-        type: "POST",
-        url: "../php/update-profile.php",
-        data: mydata,
-        success: function(response, textStatus, xhr) {
-          console.log(response);
-          if (response=="true") {
-            Materialize.toast('Profile saved', 4000);
-          }
-          else {
-            Materialize.toast('Error saving', 4000);
-          }
-        },
-        error: function(xhr, textStatus, errorThrown) {}
-    });
+    if($("input#email").hasClass('invalid')) {
+      Materialize.toast('Invalid email', 4000);
+    }
+    else {
+      var mydata = $("form#frm-profile").serialize();
+      $.ajax({
+          type: "POST",
+          url: "../php/update-profile.php",
+          data: mydata,
+          success: function(response, textStatus, xhr) {
+            console.log(response);
+            if (response=="true") {
+              Materialize.toast('Profile saved', 4000);
+            }
+            else {
+              Materialize.toast('Error saving', 4000);
+            }
+          },
+          error: function(xhr, textStatus, errorThrown) {}
+      });
+    }
     return false;
   });
 });
